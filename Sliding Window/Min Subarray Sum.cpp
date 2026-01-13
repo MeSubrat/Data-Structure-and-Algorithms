7962dcfb9adb8cbb28b2435364351291fc88383b
Subrat Kumar Sahoo <subratksahoo86@gmail.com>
2026-01-13 20:52:18 +0530

    new

---
+++ Sliding Window/Min Subarray Sum.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    int minSubArrayLen(int target, vector<int> &nums)
+    {
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+        int minL = INT_MAX;
+
+        while (j < nums.size())
+        {
+            sum += nums[j];
+            while (sum >= target)
+            {
+                minL = min(minL, j - i + 1);
+                sum -= nums[i];
+                i++;
+            }
+            j++;
+        }
+        return minL == INT_MAX ? 0 : minL;
+    }
+};
