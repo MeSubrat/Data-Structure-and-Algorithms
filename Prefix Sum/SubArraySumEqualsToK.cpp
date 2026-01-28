8309eecdc93b9a649f8ec67e2d1f4d76325cc4f9
Subrat Kumar Sahoo <subratksahoo86@gmail.com>
2026-01-28 13:35:54 +0530

    Add solution for finding subarrays that sum to a given value

---
+++ Prefix Sum/SubArraySumEqualsToK.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int subarraySum(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        int preSum = 0, cnt = 0;
+        unordered_map<int, int> mpp;
+        mpp[0] = 1;
+        for (int i = 0; i < n; i++)
+        {
+            preSum += nums[i];
+            int remove = preSum - k;
+            cnt += mpp[remove]; // This will give 0 if no element remove is present.
+            mpp[preSum] += 1;
+        }
+        return cnt;
+    }
+};
