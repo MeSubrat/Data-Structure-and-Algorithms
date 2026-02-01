f7d2d0e0f286871c6ad606c1779fb7628fe0c69e
Subrat Kumar Sahoo <subratksahoo86@gmail.com>
2026-02-01 08:39:14 +0530

    Refactor minimum cost calculation for dividing the array into subarrays

--- Array/DivideTheArrayIntoSubarraysAtMinimumCost.cpp
+++ Array/DivideTheArrayIntoSubarraysAtMinimumCost.cpp
@@ -6,8 +6,27 @@ class Solution
 public:
     int minimumCost(vector<int> &nums)
     {
-        sort(nums.begin() + 1, nums.end());
-        return nums[0] + nums[1] + nums[2];
+        // O(nlogn) O(1)
+        // sort(nums.begin()+1,nums.end());
+        // return nums[0] + nums[1] + nums[2];
+
+        // O(n) O(1)
+        int mini = INT_MAX;
+        int sMini = INT_MAX;
+
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] < mini)
+            {
+                sMini = mini;
+                mini = nums[i];
+            }
+            else if (nums[i] < sMini)
+            {
+                sMini = nums[i];
+            }
+        }
+        return nums[0] + mini + sMini;
     }
 };
 
