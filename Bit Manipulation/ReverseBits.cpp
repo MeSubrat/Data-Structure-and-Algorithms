38d25f8b101757477b80014282cf381462a12494
Subrat Kumar Sahoo <subratksahoo86@gmail.com>
2026-02-16 09:56:21 +0530

    Solution for Reverse Bits(POTD - 16th Feb)

---
+++ Bit Manipulation/ReverseBits.cpp
@@ -0,0 +1,28 @@
+//POTD - 16th FEB
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int reverseBits(int n)
+    {
+        if (n == 0)
+            return 0;
+        int result = 0;
+        for (int i = 1; i <= 32; i++)
+        {
+            result <<= 1;
+
+            result = (result | (n & 1));
+
+            n >>= 1;
+        }
+        return result;
+    }
+};
+
+int main()
+{
+}
