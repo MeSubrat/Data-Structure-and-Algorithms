f81d3b6afe5ce0c209db416a6e45371c85aa2fa2
Subrat Kumar Sahoo <subratksahoo86@gmail.com>
2026-02-17 12:46:49 +0530

    Binary Watch Solution - POTD-17th FEB

---
+++ Bit Manipulation/BinaryWatch.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// POTD - 17th FEB
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<string> result;
+        for(int HH=0;HH<=11;HH++){
+            for(int MM=0;MM<=59;MM++){
+                if(__builtin_popcount(HH) + __builtin_popcount(MM) == turnedOn){
+                    string hour = to_string(HH);
+                    string minute = (MM<10 ? "0" : "") + to_string(MM);
+                    result.push_back(hour+":"+minute);
+                }
+            }
+        }
+        return result;
+    }
+};
+
+int main()
+{
+    
+}
