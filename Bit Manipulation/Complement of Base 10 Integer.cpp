e35555b992766e3c9e8098cf5ce7f6a19df0eac4
Subrat Kumar Sahoo <subratksahoo86@gmail.com>
2026-03-11 22:36:19 +0530

    feat: implement solution for bitwise complement of a base 10 integer

---
+++ Bit Manipulation/Complement of Base 10 Integer.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int bitwiseComplement(int n)
+    {
+        if (n == 0)
+            return 1;
+
+        int length = (int)(log2(n)) + 1;
+
+        int mask = (1 << length) - 1;
+
+        return n ^ mask;
+    }
+};
