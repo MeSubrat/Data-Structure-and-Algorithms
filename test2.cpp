0f4048f213d444d0da4d4164158b76fbf07286ff
Subrat Kumar Sahoo <subratksahoo86@gmail.com>
2026-04-18 23:39:01 +0530

    New problem

---
+++ test2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    set<pair<string,string>>transaction;
+    for(int i=0;i<n;i++){
+        string sender,receiver;
+        int timestamps, amount;
+    }
+}
