b51d0bb4cf18bdd8d3975f6de6a6681918f7d634
Subrat Kumar Sahoo <subratksahoo86@gmail.com>
2026-04-20 22:32:56 +0530

    Quick Sort

---
+++ Sorting Algorithms/QuickSort.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int getPartitionIdx(vector<int> &arr, int low, int high)
+{
+    int pivot = arr[low];
+    int left = low;
+    int right = high;
+
+    while (left < right)
+    {
+        while (left <= high && arr[left] <= pivot)
+            left++;
+        while (right > low && arr[right] > pivot)
+            right--;
+        if (left < right)
+            swap(arr[left], arr[right]);
+    }
+    swap(arr[low], arr[right]);
+    return right;
+}
+
+void quickSort(vector<int> &arr, int low, int high)
+{
+    if(low>=high) return;
+    int partitionIdx = getPartitionIdx(arr, low, high);
+    quickSort(arr, low, partitionIdx-1);
+    quickSort(arr, partitionIdx + 1, high);
+}
+
+int main()
+{
+    vector<int> arr = {5, 2, 3, 4, 6, 7};
+    quickSort(arr, 0, arr.size() - 1);
+    for (int num : arr)
+    {
+        cout << num << " ";
+    }
+}
