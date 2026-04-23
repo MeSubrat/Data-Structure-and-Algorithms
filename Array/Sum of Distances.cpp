91e57820cf2f8bb36b32a892feadde0023da0ebf
Subrat Kumar Sahoo <subratksahoo86@gmail.com>
2026-04-23 20:19:12 +0530

    update

--- Array/Sum of Distances.cpp
+++ Array/Sum of Distances.cpp
@@ -1,17 +1,24 @@
-class Solution {
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
 public:
-    vector<long long> distance(vector<int>& nums) {
+    vector<long long> distance(vector<int> &nums)
+    {
         int n = nums.size();
         unordered_map<int, vector<int>> groups;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             groups[nums[i]].push_back(i);
         }
         vector<long long> res(n);
-        for (const auto& p : groups) {
-            const auto& group = p.second;
+        for (const auto &p : groups)
+        {
+            const auto &group = p.second;
             long long total = accumulate(group.begin(), group.end(), 0LL);
             long long prefixTotal = 0;
-            for (int i = 0; i < group.size(); i++) {
+            for (int i = 0; i < group.size(); i++)
+            {
                 res[group[i]] =
                     total - prefixTotal * 2 + group[i] * (2 * i - group.size());
                 prefixTotal += group[i];
@@ -20,3 +27,7 @@ public:
         return res;
     }
 };
+
+int main()
+{
+}
