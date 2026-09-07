6ea0b6e85486c430cc2717f8485e5facfa5eaef9
Subrat kumar Sahoo <113829258+MeSubrat@users.noreply.github.com>
2026-09-07 17:29:27 +0530

    Time: 4 ms (37.73%), Space: 28.4 MB (46.84%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,7 +1,8 @@
 class Solution {
 private:
-    int dfs(int row, int col, vector<vector<int>> &vis, vector<vector<int>> &grid){
-        int area = 1;
+    void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<int>> &grid, int &cnt){
+        // int area = 1;
+        cnt++;
         vis[row][col] = 1;
         int n = grid.size();
         int m = grid[0].size();
@@ -16,10 +17,11 @@ private:
             ncol >= 0 && ncol < m &&
             grid[nrow][ncol] == 1 &&
             !vis[nrow][ncol]) {
-                area += dfs(nrow, ncol, vis, grid);
+                // area += dfs(nrow, ncol, vis, grid);
+                dfs(nrow, ncol, vis, grid,cnt);
             }
         }
-        return area;
+        // return area;
 
     }
 public:
@@ -30,15 +32,19 @@ public:
         vector<vector<int>> vis(n, vector<int>(m,0));
         int area = 0;
         int maxArea = 0;
+        int cnt = 0;
+        int maxCnt = 0;
         for(int row = 0;row<n;row++){
             for(int col = 0;col<m;col++){
                 if(!vis[row][col] && grid[row][col] == 1){
-                    // cnt++;
-                    area = dfs(row, col, vis, grid);
-                    maxArea = max(area, maxArea);
+                    // area = dfs(row, col, vis, grid);
+                    cnt = 0;
+                    dfs(row, col, vis, grid,cnt);
+                    // maxArea = max(area, maxArea);
                 }
+                maxCnt = max(maxCnt,cnt);
             }
         }
-        return maxArea;
+        return maxCnt;
     }
 };
